804c22cf9f00eb6e19d43a3938dc4c1a6aab9a0c
Jason Leon <jasonleon01@outlook.com>
2025-02-12 20:45:45 +0800

    Initial commit

---
+++ Base/__init__.py
@@ -0,0 +1,27 @@
+from .System import (
+    Angle,
+    Time,
+    Clock,
+    Vector2f,
+    Vector2i,
+    Vector2u,
+    Vector3f,
+    Vector3i,
+    Vector3u,
+    InputStream,
+    FileInputStream
+)
+
+__all__ = [
+    'Angle',
+    'Time',
+    'Clock',
+    'Vector2f',
+    'Vector2i',
+    'Vector2u',
+    'Vector3f',
+    'Vector3i',
+    'Vector3u',
+    'InputStream',
+    'FileInputStream',
+]
